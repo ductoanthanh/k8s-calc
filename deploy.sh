c6d79b93288f56a61e0c4ce6516211fe24db3c0c
Duc Toan Thanh <toanth@metropolia.fi>
2020-01-07 17:15:47 +0200

    Change images' names

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-docker build -t toanthanh/multi-client:latest -t toanthanh/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t toanthanh/multi-server:latest -t toanthanh/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t toanthanh/multi-worker:latest -t toanthanh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t toanthanh/fib-client:latest -t toanthanh/fib-client:$SHA -f ./client/Dockerfile ./client
+docker build -t toanthanh/fib-server:latest -t toanthanh/fib-server:$SHA -f ./server/Dockerfile ./server
+docker build -t toanthanh/fib-worker:latest -t toanthanh/fib-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push toanthanh/multi-client:latest
-docker push toanthanh/multi-server:latest
-docker push toanthanh/multi-worker:latest
+docker push toanthanh/fib-client:latest
+docker push toanthanh/fib-server:latest
+docker push toanthanh/fib-worker:latest
 
-docker push toanthanh/multi-client:$SHA
-docker push toanthanh/multi-server:$SHA
-docker push toanthanh/multi-worker:$SHA
+docker push toanthanh/fib-client:$SHA
+docker push toanthanh/fib-server:$SHA
+docker push toanthanh/fib-worker:$SHA
